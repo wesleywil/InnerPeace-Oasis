d1663f50364ae2158b6e000fd68d316f32b15f6a
wesleywil <wesleywilsonti@gmail.com>
2023-09-21 18:58:07 -0300

    update homepage and added meditate page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import "../app.css";
 </script>
 
-<div>
+<div class="bg-[#fdfefd]">
   <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
-<div class="mb-4 text-3xl font-bold">
-  <h1>Hello World!</h1>
-  <h2>H</h2>
-</div>
+<main
+  class="min-h-screen min-w-screen p-2 flex flex-col items-center justify-center gap-2 text-[#292929] text-center"
+>
+  <h1
+    class="w-full md:w-2/3 xl:w-1/3 pb-1 flex justify-center items-center text-6xl font-bold border-b-2 border-[#24bb49]"
+  >
+    <span class="text-[#24bb49] text-7xl">I</span>nnerPeace
+    <span class="ml-2 text-[#24bb49] text-7xl">O</span>asis
+  </h1>
+  <h2 class="md:w-2/3 xl:w-1/3 mt-2 px-2 text-xl md:text-3xl font-semibold">
+    Find Tranquility, Relaxation, and Balance in the Digital Sanctuary of
+    InnerPeace Oasis
+  </h2>
+  <button
+    class="md:mt-4 px-2 py-1 text-[#fdfefd] text-3xl md:text-5xl bg-[#24bb49] hover:bg-[#07681f] font-semibold rounded-xl transform duration-700 ease-in-out"
+    >Meditate</button
+  >
+</main>
