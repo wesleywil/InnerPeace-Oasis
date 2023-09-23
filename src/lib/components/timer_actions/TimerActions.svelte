30b9d189469cff6f020241ad323a662c6ef09d56
wesleywil <wesleywilsonti@gmail.com>
2023-09-23 14:18:50 -0300

    updated TimerActions component by including functions that will play the meditation songs

--- src/lib/components/timer_actions/TimerActions.svelte
+++ src/lib/components/timer_actions/TimerActions.svelte
@@ -1,20 +1,68 @@
 <script lang="ts">
   import { timer } from "../../../store/timer";
+
+  import music_5_minutes from "$lib/assets/musics/5m.mp3";
+  import music_10_minutes from "$lib/assets/musics/10m.mp3";
+  import music_15_minutes from "$lib/assets/musics/15m.mp3";
+  import music_20_minutes from "$lib/assets/musics/20m.mp3";
+
+  let toggle: boolean = false;
+  $: toggle = $timer.toggle;
+  let count: number = 0;
+  $: count = $timer.count;
+
+  const play = () => {
+    if (count !== 0) {
+      timer.startTimer();
+      const music = document.getElementById("music") as HTMLAudioElement | null;
+      if (!toggle) {
+        music!.play();
+      }
+    }
+  };
+
+  const stop = () => {
+    timer.stopTimer();
+    const music = document.getElementById("music") as HTMLAudioElement | null;
+    if (toggle) {
+      music!.pause();
+    }
+  };
+
+  const reset = () => {
+    timer.resetTimer();
+    const music = document.getElementById("music") as HTMLAudioElement | null;
+    if (toggle) {
+      timer.setToggle(false);
+      music!.pause();
+      music!.currentTime = 0;
+    }
+  };
 </script>
 
 <div class="flex justify-center gap-2">
+  {#if count <= 300}
+    <audio id="music" src={music_5_minutes} />
+  {:else if count > 300 && count <= 600}
+    <audio id="music" src={music_10_minutes} />
+  {:else if count > 600 && count <= 900}
+    <audio id="music" src={music_15_minutes} />
+  {:else}
+    <audio id="music" src={music_20_minutes} />
+  {/if}
+
   <button
-    on:click={() => timer.startTimer()}
+    on:click={play}
     class="md:mt-4 px-2 py-1 text-[#fdfefd] text-3xl md:text-5xl bg-[#24bb49] hover:bg-[#07681f] font-semibold rounded-xl transform duration-700 ease-in-out"
     >Start</button
   >
   <button
-    on:click={() => timer.stopTimer()}
+    on:click={stop}
     class="md:mt-4 px-2 py-1 text-[#fdfefd] text-3xl md:text-5xl bg-[#24bb49] hover:bg-[#07681f] font-semibold rounded-xl transform duration-700 ease-in-out"
     >Stop</button
   >
   <button
-    on:click={() => timer.resetTimer()}
+    on:click={reset}
     class="md:mt-4 px-2 py-1 text-[#fdfefd] text-3xl md:text-5xl bg-[#24bb49] hover:bg-[#07681f] font-semibold rounded-xl transform duration-700 ease-in-out"
     >Reset</button
   >
