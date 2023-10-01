7407033f23f2a53810ee74536685cd31aab6a447
wesleywil <wesleywilsonti@gmail.com>
2023-10-01 18:25:36 -0300

    some adjutments in the authentication adapter and added a verification the all pages to handle when the user iss not authenticated

--- src/lib/auth/adapter.ts
+++ src/lib/auth/adapter.ts
@@ -1,113 +1,129 @@
-import type { Adapter, AdapterSession, AdapterUser, VerificationToken } from "next-auth/adapters";
+import type {
+  Adapter,
+  AdapterSession,
+  AdapterUser,
+  VerificationToken,
+} from "@auth/core/adapters";
 import { mysqlconnFn } from "$lib/db/mysql";
 import type { Connection } from "mysql2/promise";
 
-
-export function mapExpiresAt(account:any):any{
-    const expires_at:number = parseInt(account.expires_at)
-    return {
-        ...account,
-        expires_at,
-    }
+export function mapExpiresAt(account: any): any {
+  const expires_at: number = parseInt(account.expires_at);
+  return {
+    ...account,
+    expires_at,
+  };
 }
 
-export default function MysqlAdapter(client:Connection):Adapter{
-    return{
-        async createVerificationToken(
-            verificationToken:VerificationToken
-        ):Promise<VerificationToken>{
-            const {identifier, expires, token} = verificationToken
-            const sql = `INSERT INTO innerpease_oasis.verification_token (identifier, expires, token) VALUES ($1, $2, $3)`
-            await client.query(sql, [identifier, expires, token])
-            return verificationToken
-        },
-        async useVerificationToken({
-            identifier,
-            token,
-        }:{
-            identifier:string,
-            token:string
-        }):Promise<VerificationToken>{
-            const sql = `DELETE FROM innerpease_oasis.verification_token WHERE identifier = $1 and token = $2 RETURNING identifier, expires, token`
-            const result = await client.query(sql, [identifier, token])
-            return result.rowCount !== 0 ? result.rows[0]:null
-        },
-        async createUser(user: Omit<AdapterUser, "id">) {
-            const { name, email, emailVerified, image } = user
-            const sql = `
-              INSERT INTO innerpease_oasis.users (name, email, "emailVerified", image) 
-              VALUES ($1, $2, $3, $4) 
-              RETURNING id, name, email, "emailVerified", image`
-            const result = await client.query(sql, [
-              name,
-              email,
-              emailVerified,
-              image,
-            ])
-            return result.rows[0]
-          },
-          async getUser(id) {
-            const sql = `select * from innerpease_oasis.users where id = $1`
-            try {
-              const result = await client.query(sql, [id])
-              return result.rowCount === 0 ? null : result.rows[0]
-            } catch (e) {
-              return null
-            }
-          },
-          async getUserByEmail(email) {
-            const sql = `select * from innerpease_oasis.users where email = $1`
-            const result = await client.query(sql, [email])
-            return result.rowCount !== 0 ? result.rows[0] : null
-          },
-          async getUserByAccount({
-            providerAccountId,
-            provider,
-          }): Promise<AdapterUser | null> {
-            const sql = `
-                select u.* from innerpease_oasis.users u join innerpease_oasis.accounts a on u.id = a."userId"
-                where 
-                a.provider = $1 
-                and 
-                a."providerAccountId" = $2`
+export default function MysqlAdapter(client: Connection): Adapter {
+  return {
+    async createVerificationToken(
+      verificationToken: VerificationToken
+    ): Promise<VerificationToken> {
+      const { identifier, expires, token } = verificationToken;
+      const sql = `INSERT INTO innerpease_oasis.verificationtoken (identifier, expires, token) VALUES ("${identifier}", "${expires}", "${token}");`;
+      await client.query(sql);
+      return verificationToken;
+    },
+    async useVerificationToken({
+      identifier,
+      token,
+    }: {
+      identifier: string;
+      token: string;
+    }): Promise<VerificationToken> {
+      const sql = `DELETE FROM innerpease_oasis.verificationtoken WHERE identifier = "${identifier}" and token = "${token}";`;
+      const result = await client.query(sql);
+      return result.rowCount !== 0 ? result.rows[0] : null;
+    },
+    async createUser(user: Omit<AdapterUser, "id">) {
+      const { name, email, emailVerified, image } = user;
+      // const sql = `
+      //         INSERT INTO innerpease_oasis.user (name, email, emailVerified, image) 
+      //         VALUES ("${name}", "${email}", "${emailVerified}", "${image}");`;
+      const sql = `INSERT INTO innerpease_oasis.user (name, email, emailVerified, image) VALUES(?,?,?,?)ON DUPLICATE KEY UPDATE name = VALUES(name), emailVerified = VALUES(emailVerified), image = VALUES(image)`;
+      const values = [name, email, emailVerified, image];
+      try{
+        await client.query(sql, values);
+        const [UserData] = await client.query('SELECT * FROM innerpease_oasis.user WHERE email = ?', [email])
+        return UserData;
+
+      }catch(error){
+        console.error("Got an error");
+        console.log(error);
+        throw error
+      }
       
-            const result = await client.query(sql, [provider, providerAccountId])
-            return result.rowCount !== 0 ? result.rows[0] : null
-          },
-          async updateUser(user: Partial<AdapterUser>): Promise<AdapterUser> {
-            const fetchSql = `select * from innerpease_oasis.users where id = $1`
-            const query1 = await client.query(fetchSql, [user.id])
-            const oldUser = query1.rows[0]
-      
-            const newUser = {
-              ...oldUser,
-              ...user,
-            }
-      
-            const { id, name, email, emailVerified, image } = newUser
-            const updateSql = `
-              UPDATE innerpease_oasis.users set
-              name = $2, email = $3, "emailVerified" = $4, image = $5
-              where id = $1
-              RETURNING name, id, email, "emailVerified", image
-            `
-            const query2 = await client.query(updateSql, [
-              id,
-              name,
-              email,
-              emailVerified,
-              image,
-            ])
-            return query2.rows[0]
-          },
-          async linkAccount(account) {
-            const sql = `
-            insert into innerpease_oasis.accounts 
+    },
+    async getUser(id) {
+      const sql = `select * from innerpease_oasis.user where id = ${id};`;
+      try {
+        const result = await client.query(sql);
+        return result.rowCount === 0 ? null : result.rows[0];
+      } catch (e) {
+        return null;
+      }
+    },
+    async getUserByEmail(email) {
+      const sql = `select * from innerpease_oasis.user where email = "${email}";`;
+      console.log('EMAIL SQL=> ', sql)
+      const result = await client.query(sql);
+      if(result.rows && result.rows.length >0){
+        return result.rows[0]
+      }else{
+        return null;
+      }
+    },
+    async getUserByAccount({
+      providerAccountId,
+      provider,
+    }): Promise<AdapterUser | null> {
+      try {
+        const sql = `
+        select u.* from innerpease_oasis.user u join innerpease_oasis.account a on u.id = a.userId
+        where 
+        a.provider = "${provider}" 
+        and 
+        a.providerAccountId = "${providerAccountId}";`;
+        const result = await client.query(sql);
+        if(result.rows && result.rows.length > 0){
+          return result.rows[0]
+        }else{
+          return null;
+        }
+       
+      } catch (error) {
+        console.error("Got an error");
+        console.log(error);
+        throw error;
+      }
+    },
+    async updateUser(user: Partial<AdapterUser>): Promise<AdapterUser> {
+      const fetchSql = `select * from innerpease_oasis.user where id = ${user.id};`;
+      const query1 = await client.query(fetchSql);
+      const oldUser = query1.rows[0];
+
+      const newUser = {
+        ...oldUser,
+        ...user,
+      };
+
+      const { id, name, email, emailVerified, image } = newUser;
+      const updateSql = `
+              UPDATE innerpease_oasis.user set
+              name = "${name}", email = "${email}", emailVerified = "${emailVerified}", image = "${image}"
+              where id = ${id};`;
+      const query2 = await client.query(updateSql);
+      return query2.rows[0];
+    },
+    async linkAccount(account) {
+      const sql = `
+            insert into innerpease_oasis.account 
             (
-              "userId", 
+              userId, 
               provider, 
               type, 
-              "providerAccountId", 
+              providerAccountId, 
               access_token,
               expires_at,
               refresh_token,
@@ -116,120 +132,91 @@ export default function MysqlAdapter(client:Connection):Adapter{
               session_state,
               token_type
             )
-            values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
-            returning
-              id,
-              "userId", 
-              provider, 
-              type, 
-              "providerAccountId", 
-              access_token,
-              expires_at,
-              refresh_token,
-              id_token,
-              scope,
-              session_state,
-              token_type
-            `
-      
-            const params = [
-              account.userId,
-              account.provider,
-              account.type,
-              account.providerAccountId,
-              account.access_token,
-              account.expires_at,
-              account.refresh_token,
-              account.id_token,
-              account.scope,
-              account.session_state,
-              account.token_type,
-            ]
-      
-            const result = await client.query(sql, params)
-            return mapExpiresAt(result.rows[0])
-          },
-          async createSession({ sessionToken, userId, expires }) {
-            if (userId === undefined) {
-              throw Error(`userId is undef in createSession`)
-            }
-            const sql = `insert into innerpease_oasis.sessions ("userId", expires, "sessionToken")
-            values ($1, $2, $3)
-            RETURNING id, "sessionToken", "userId", expires`
-      
-            const result = await client.query(sql, [userId, expires, sessionToken])
-            return result.rows[0]
-          },
-      
-          async getSessionAndUser(sessionToken: string | undefined): Promise<{
-            session: AdapterSession
-            user: AdapterUser
-          } | null> {
-            if (sessionToken === undefined) {
-              return null
-            }
-            const result1 = await client.query(
-              `select * from innerpease_oasis.sessions where "sessionToken" = $1`,
-              [sessionToken]
-            )
-            if (result1.rowCount === 0) {
-              return null
-            }
-            let session: AdapterSession = result1.rows[0]
-      
-            const result2 = await client.query("select * from innerpease_oasis.users where id = $1", [
-              session.userId,
-            ])
-            if (result2.rowCount === 0) {
-              return null
-            }
-            const user = result2.rows[0]
-            return {
-              session,
-              user,
-            }
-          },
-          async updateSession(
-            session: Partial<AdapterSession> & Pick<AdapterSession, "sessionToken">
-          ): Promise<AdapterSession | null | undefined> {
-            const { sessionToken } = session
-            const result1 = await client.query(
-              `select * from innerpease_oasis.sessions where "sessionToken" = $1`,
-              [sessionToken]
-            )
-            if (result1.rowCount === 0) {
-              return null
-            }
-            const originalSession: AdapterSession = result1.rows[0]
-      
-            const newSession: AdapterSession = {
-              ...originalSession,
-              ...session,
-            }
-            const sql = `
-              UPDATE innerpease_oasis.sessions set
-              expires = $2
-              where "sessionToken" = $1
-              `
-            const result = await client.query(sql, [
-              newSession.sessionToken,
-              newSession.expires,
-            ])
-            return result.rows[0]
-          },
-          async deleteSession(sessionToken) {
-            const sql = `delete from innerpease_oasis.sessions where "sessionToken" = $1`
-            await client.query(sql, [sessionToken])
-          },
-          async unlinkAccount(partialAccount) {
-            const { provider, providerAccountId } = partialAccount
-            const sql = `delete from innerpease_oasis.accounts where "providerAccountId" = $1 and provider = $2`
-            await client.query(sql, [providerAccountId, provider])
-          },
-          async deleteUser(userId: string) {
-            await client.query(`delete from innerpease_oasis.users where id = $1`, [userId])
-            await client.query(`delete from innerpease_oasis.sessions where "userId" = $1`, [userId])
-            await client.query(`delete from innerpease_oasis.accounts where "userId" = $1`, [userId])
-          },
-    }
-}   
+            values (${account.userId}, "${account.provider}", "${account.type}", "${account.providerAccountId}", "${account.access_token}", ${account.expires_at}, "${account.refresh_token}", "${account.id_token}", "${account.scope}", "${account.session_state}", "${account.token_type}");`;
+              console.log('LINK ACCOUNT QUERY => ', sql);
+            const result = await client.query(sql);
+      return mapExpiresAt(result.rows[0]);
+    },
+    async createSession({ sessionToken, userId, expires }) {
+      if (userId === undefined) {
+        throw Error(`userId is undef in createSession`);
+      }
+      const sql = `insert into innerpease_oasis.session (userId, expires, sessionToken)
+            values (${userId}, "${expires}", "${sessionToken}");`;
+
+      const result = await client.query(sql);
+      return result.rows[0];
+    },
+
+    async getSessionAndUser(sessionToken: string | undefined): Promise<{
+      session: AdapterSession;
+      user: AdapterUser;
+    } | null> {
+      if (sessionToken === undefined) {
+        return null;
+      }
+      const result1 = await client.query(
+        `select * from innerpease_oasis.session where sessionToken = "${sessionToken}";`
+      );
+      if (result1.rowCount === 0) {
+        return null;
+      }
+      let session: AdapterSession = result1.rows[0];
+
+      const result2 = await client.query(
+        `select * from innerpease_oasis.user where id = ${session.userId};`
+      );
+      if (result2.rowCount === 0) {
+        return null;
+      }
+      const user = result2.rows[0];
+      return {
+        session,
+        user,
+      };
+    },
+    async updateSession(
+      session: Partial<AdapterSession> & Pick<AdapterSession, "sessionToken">
+    ): Promise<AdapterSession | null | undefined> {
+      const { sessionToken } = session;
+      const result1 = await client.query(
+        `select * from innerpease_oasis.session where sessionToken = "${sessionToken}";`
+      );
+      if (result1.rowCount === 0) {
+        return null;
+      }
+      const originalSession: AdapterSession = result1.rows[0];
+
+      const newSession: AdapterSession = {
+        ...originalSession,
+        ...session,
+      };
+      const sql = `
+              UPDATE innerpease_oasis.session set
+              expires = "${newSession.expires}"
+              where sessionToken = "${newSession.sessionToken}";`;
+      const result = await client.query(sql);
+      return result.rows[0];
+    },
+    async deleteSession(sessionToken) {
+      const sql = `delete from innerpease_oasis.session where sessionToken = ${sessionToken};`;
+      await client.query(sql);
+    },
+    async unlinkAccount(partialAccount) {
+      const { provider, providerAccountId } = partialAccount;
+      const sql = `delete from innerpease_oasis.account where providerAccountId = "${providerAccountId}" and provider = "${provider}";`;
+      await client.query(sql);
+    },
+    async deleteUser(userId: string) {
+      await client.query(
+        `delete from innerpease_oasis.user where id = ${userId};`
+      );
+      await client.query(
+        `delete from innerpease_oasis.session where userId = $${userId};`
+      );
+      await client.query(
+        `delete from innerpease_oasis.account where userId = $${userId};`
+      );
+    },
+  };
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,43 @@
 <script>
+  import { page } from "$app/stores";
+  import { signIn } from "@auth/sveltekit/client";
+
   import Menu from "$lib/components/menu/Menu.svelte";
+  import NotAuthorized from "$lib/components/not_authorized/NotAuthorized.svelte";
   import "../app.css";
 </script>
 
-<div class="bg-[#fdfefd] pattern6">
+<svelte:head>
+  <title>InnerPeace Oasis</title>
+</svelte:head>
+
+<!-- <div class="bg-[#fdfefd] pattern6">
   <Menu />
   <slot />
-</div>
+</div> -->
+
+{#if $page.data.session && Object.keys($page.data.session).length}
+  <div class="bg-[#fdfefd] pattern6">
+    <Menu />
+    <slot />
+  </div>
+{:else}
+  <main
+    class="min-h-screen min-w-screen p-2 flex flex-col items-center justify-center gap-2 text-[#292929] text-center bg-[#fdfefd] pattern6"
+  >
+    <div class="p-2 flex flex-col bg-[#292929]/80 rounded">
+      <h1 class="text-2xl text-[#fdfefd] font-bold">
+        YOU NEED TO BE LOGGED IN TO ACCESS THIS PAGE
+      </h1>
+
+      <button
+        on:click={() => signIn("google")}
+        class="mt-2 px-2 py-1 text-xl font-semibold bg-[#fdfefd] hover:bg-[#24bb49] rounded transform duration-700 ease-in-out"
+        >SignUp/SignIn</button
+      >
+    </div>
+  </main>
+{/if}
 
 <style>
   .pattern6 {
