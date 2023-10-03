c93110276dc6a584a3666ab954b6816e04bd2a5e
wesleywil <wesleywilsonti@gmail.com>
2023-10-03 15:41:21 -0300

    added some information and signOut button to logged In users

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import { signOut } from "@auth/sveltekit/client";
   import type { PageData } from "./$types";
-  import PercentageYear from "$lib/components/percentage_year/PercentageYear.svelte";
+  import { page } from "$app/stores";
+
   import type { MeditationData } from "../../utils/interfaces";
+  import PercentageYear from "$lib/components/percentage_year/PercentageYear.svelte";
   import PercentageMonths from "$lib/components/pencentage_months/PercentageMonths.svelte";
 
   export let data: PageData;
@@ -25,7 +28,28 @@
   <div
     class="w-full md:w-2/3 xl:w-1/3 my-2 p-1 flex flex-col justify-center items-center text-[#fdfefd] bg-[#292929]/70 rounded"
   >
-    <h1 class="text-2xl md:text-4xl font-bold">Dashboard</h1>
+    <h1
+      class="mb-2 pb-2 text-2xl md:text-4xl font-bold border-b-2 border-[#fdfefd]"
+    >
+      Dashboard
+    </h1>
+    {#if $page.data.session}
+      {#if $page.data.session.user?.image}
+        <img
+          src={$page.data.session.user.image}
+          alt="user"
+          class="w-12 md:w-24 h-12 md:h-24 rounded-full"
+        />
+      {/if}
+      <div class="mb-2 flex flex-col gap-2">
+        <h2 class="md:text-xl">{$page.data.session.user?.name}</h2>
+        <button
+          on:click={() => signOut()}
+          class="md:w-24 mx-auto px-2 md:px-0 py-1 font-bold bg-[#24bb49] rounded"
+          >Sign Out</button
+        >
+      </div>
+    {/if}
   </div>
   <!-- Options -->
   <div class="w-full md:w-2/3 xl:w-1/3 p-2 flex flex-col rounded-t bg-black/70">
