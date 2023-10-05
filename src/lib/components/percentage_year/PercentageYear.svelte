197fdc929920567ef46d50702025283fdc05e96d
wesleywil <wesleywilsonti@gmail.com>
2023-10-05 13:33:12 -0300

    fixed PercentageYear component by displaying the information about meditation session correctly

--- src/lib/components/percentage_year/PercentageYear.svelte
+++ src/lib/components/percentage_year/PercentageYear.svelte
@@ -4,39 +4,49 @@
 
   export let data: MeditationData[];
 
-  const numberOfDaysMeditated = data.length || 0;
+  // Initialize year-wise meditation counts
+  const yearWiseMeditationCounts: { [key: string]: number } = {};
 
-  const percentageInYear = (numberOfDaysMeditated / 365) * 100;
+  // Calculate year-wise meditation counts
+  data.forEach((dataSession) => {
+    const year = formatDate(dataSession.end_time).slice(6, 12);
+
+    // Count meditations
+    if (year in yearWiseMeditationCounts) {
+      yearWiseMeditationCounts[year]++;
+    } else {
+      yearWiseMeditationCounts[year] = 1;
+    }
+  });
+
+  // Extract the years as an array
+  const years = Object.keys(yearWiseMeditationCounts);
+
+  // Calculate the total number of days meditated (count of data items)
+  const totalDaysMeditated = data.length;
+
+  // Calculate percentage in year for each year
+  const percentageInYear: { [key: string]: number } = {};
+  years.forEach((year) => {
+    percentageInYear[year] =
+      (yearWiseMeditationCounts[year] / totalDaysMeditated) * 100;
+  });
 </script>
 
 <div class="w-full md:w-2/3 xl:w-1/3 h-96 p-2 overflow-y-auto">
-  {#if data !== undefined && data !== null}
-    {#if data.length === 0}
-      <p class="my-4 font-bold text-2xl font-semibold text-[#fdfefd]">
-        No meditations were registered
-      </p>
-    {/if}
-    {#each data as dataSession (dataSession.userId)}
-      <div
-        class="flex bg-black rounded text-white md:text-2xl rounded border overflow-hidden"
-      >
-        <div class="p-2 flex items-center border-r">
-          <h1>
-            {formatDate(dataSession.end_time).slice(6, 12)}
-          </h1>
-        </div>
-
-        <h1 class="p-2 border-r">
-          Days Meditated: {numberOfDaysMeditated}
-        </h1>
-        <h1 class="p-2">
-          Percentage in the Year: {percentageInYear.toFixed(2)}%
-        </h1>
+  {#each years as year}
+    <div
+      class="px-2 py-1 flex justify-between text-2xl text-[#fdfefd] bg-[#292929] border-2 border-[#fdfefd] rounded"
+    >
+      <div class="pr-2 border-r-2">
+        <h1>{year}</h1>
       </div>
-    {/each}
-  {:else}
-    <p class="my-4 font-bold text-2xl font-semibold text-[#fdfefd]">
-      No meditations were registered
-    </p>
-  {/if}
+      <div class="pr-2 border-r-2">
+        <h1>Days Meditated: {yearWiseMeditationCounts[year]}</h1>
+      </div>
+      <div>
+        <h1>Percentage in the Year: {percentageInYear[year]}%</h1>
+      </div>
+    </div>
+  {/each}
 </div>
