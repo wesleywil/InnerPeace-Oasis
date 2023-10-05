798798c795369c9c58cab2c22cd26fccc1af1346
wesleywil <wesleywilsonti@gmail.com>
2023-10-05 13:32:34 -0300

    updated routes page.server.ts of meditiation and dashboard by query the meditation info by logged In user id

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -2,18 +2,23 @@ import { mysqlconnFn } from "$lib/db/mysql";
 import type { MeditationData } from "../../utils/interfaces";
 
 
-export async function load():Promise<{data:MeditationData[] | null}|Error>{
+export async function load({locals:{getSession}}):Promise<{data:MeditationData[] | null}|Error>{
+    const session = await getSession();
     try{
-        const mysqlconn = await mysqlconnFn();
-        const [rows, _] = await mysqlconn.query("SELECT*FROM innerpease_oasis.meditation_sessions;");
-        if(rows.length === 0){
-            return{
-                data:null
+        if(session){
+            const mysqlconn = await mysqlconnFn();
+            const [rows, _] = await mysqlconn.query("SELECT*FROM innerpease_oasis.meditation_sessions WHERE userId = ?;", [session.user?.id]);
+            if(rows.length === 0){
+                return{
+                    data:null
+                }
+            }
+            return {
+                data:rows
             }
         }
-        return {
-            data:rows
-        }
+        return {data:null}
+       
     }catch(error:any){
     console.error('Got and error!!!');
     console.log(error);
--- src/routes/meditate/+page.server.ts
+++ src/routes/meditate/+page.server.ts
@@ -5,20 +5,23 @@ interface UserData{
     id:number
 }
 
-export async function load():Promise<{data:UserData | null} | Error>{
-   try{
-    const mysqlconn = await mysqlconnFn();
-    const [rows, _] = await mysqlconn.query("SELECT name, id FROM innerpease_oasis.user LIMIT 1;");
-    if(rows.length === 0){
+export async function load({locals:{getSession}}):Promise<{data:UserData | null} | Error>{
+   const session = await getSession();
+    try{
+    if(session){
+        const mysqlconn = await mysqlconnFn();
+        const [rows, _] = await mysqlconn.query("SELECT name, id FROM innerpease_oasis.user WHERE id = ?;", [session.user?.id]);
+        if(rows.length === 0){
+            return {
+                data:null,
+            }
+        }
+        const firstRow:UserData = rows[0];
         return {
-            data:null,
+            data:firstRow,
         }
     }
-    const firstRow:UserData = rows[0];
-    console.log(firstRow);
-    return {
-        data:firstRow,
-    }
+    return {data:null}
    }catch(error:any){
     console.error('Got and error!!!');
     console.log(error);
